121606633c3ca5baa8ef11eacad2f88d634af809
Gabriel <gabriel99luques@gmail.com>
2022-06-09 17:36:32 -0300

    Add loading and error images

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -10,7 +10,8 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         imgView.load(imgUri){
-
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
         }
     }
 }
