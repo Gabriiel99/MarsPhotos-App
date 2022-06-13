cbcad74a4031efe9149d52714cf9a7e8d645df34
Gabriel <gabriel99luques@gmail.com>
2022-06-13 17:54:03 -0300

    Add the binding adapter and connect the parts

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -3,7 +3,10 @@ package com.example.android.marsphotos
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -13,5 +16,12 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             placeholder(R.drawable.loading_animation)
             error(R.drawable.ic_broken_image)
         }
+    }
+
+    @BindingAdapter("listData")
+    fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsPhoto>?) {
+        val adapter = recyclerView.adapter as PhotoGridAdapter
+        adapter.submitList(data)
+
     }
 }
